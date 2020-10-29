db5b26d0b6b09c4b4cfc58238b57d3014ce57aae
Jared Strohschein <jared.strohschein@gmail.com>
2020-10-29 18:54:07 -0400

    MVP

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const shortid = require('shortid')
 const server = express()
 server.use(express.json())
 
+
+
+
 const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
@@ -11,15 +14,7 @@ server.listen(PORT, () => {
 
 let users = []
 
-
-
-// server.get('/', (req, res) => {
-//   res.json({ hello: 'World' })
-// })
-
-// server.get('/hello', (req, res) => {
-//   res.json({ response: 'Hello There!' })
-// })
+const { check, validationResult } = require('express-validator')
 
 
 //C - create (POST)
@@ -29,21 +24,32 @@ let users = []
 
 
 //CREATE
-//need a METHOD and a PATH
-server.post('/api/users', (req, res) => {
+server.post('/api/users', [
 
-  const userInfo = req.body
-  userInfo.id = shortid.generate
-  users.push(userInfo)
+  check('name').isLength({ min: 1 }),
+  check('bio').isLength({ min: 1 })
 
-  res.status(201).json(userInfo)
+], (req, res) => {
+
+  const errors = validationResult(req)
+
+  if(!errors.isEmpty()){
+    res.status(400).json({ message: "please provide name and bio for the user" })
+  }
+
+  else{
+    const userInfo = req.body
+    userInfo.id = shortid.generate()
+    users.push(userInfo)
+    res.status(201).json(userInfo)
+  }
 
 })
 
 
 //READ
 server.get('/api/users', (req, res) => {
-  res.status(200).json
+  res.status(200).json(users)
 })
 
 server.get('/api/users/:id', (req, res) => {
@@ -57,23 +63,38 @@ server.get('/api/users/:id', (req, res) => {
       .json(user)
   } else {
     res
-      .json({ message: 'user not found' })
+      .status(404)
+      .json({ message: 'The user with the specified ID does not exist' })
   }
 })
 
 
 //UPDATE
-server.put('/api/users/:id', (req, res) => {
-  const {id} = req.params
-  const changes = req.body
+server.put('/api/users/:id', [
 
-  let index = users.findIndex(user => user.id === id)
+  check('name').isLength({ min: 1 }),
+  check('bio').isLength({ min: 1 })
 
-  if (index !== -1){
-    users[index] = changes
-    res.status(200).json(users[index])
-  } else {
-    res.status(404).json({ message: 'id not found' })
+], (req, res) => {
+
+
+  if(!errors.isEmpty()){
+    res.status(400).json({ message: "please provide name and bio for the user" })
+  }
+
+  else{
+
+    const {id} = req.params
+    const changes = req.body
+  
+    let index = users.findIndex(user => user.id === id)
+  
+    if (index !== -1){
+      users[index] = changes
+      res.status(200).json(users[index])
+    } else {
+      res.status(404).json({ message: 'id not found' })
+    }
   }
 })
 
@@ -101,10 +122,11 @@ server.delete('/api/users/:id', (req, res) => {
 
   if (deleteItem) {
     users = users.filter(user => user.id !== id)
+    res.json(deleteItem)
   } 
   
   else {
-    res.status(404).json({ message: 'id not found' })
+    res.status(404).json({ message: 'The user with the specified ID does not exist.' })
   }
 
 })
